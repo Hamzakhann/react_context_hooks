4cfc651e0d26bf74a6126b5607479edc93a631a4
Hamzakhann <hamzakhann66@gmail.com>
2019-07-09 10:40:26 +0100

    mutiple contexts

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle  from './components/ThemeToggle';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
+
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-      <ThemeToggle/>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
-
     </div>
   );
 }
